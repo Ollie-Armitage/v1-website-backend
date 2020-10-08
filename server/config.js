bee7983dcd5f8041231d124da17d1d29a06e56dd
ollie <oliverrarmitage@gmail.com>
2020-10-08 17:02:04 +0100

    Added environment variables to dblink string.

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 exports.dblink = "mongodb+srv://ollie-0:"
-        + process.argv[2]
+        + process.env.dbpassword
         + "@website-cluster-0.5nllz.azure.mongodb.net/"
-        + process.argv[3]
+        + process.env.dbname
         + "?retryWrites=true&w=majority"
 
 // Need to enter password and dbname into node args.
