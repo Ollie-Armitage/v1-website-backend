019e87ab6ea9e8bdb06a56938d40d731cb386a7a
ollie <oliverrarmitage@gmail.com>
2020-10-08 16:50:00 +0100

    Changed port to 8080

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ const blogs = require('./server/routes/api/blog');
 app.use('/api/submits', submits);
 app.use('/api/blog', blogs);
 
-const port = process.env.port || 5000;
+const port = process.env.port || 8080;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
